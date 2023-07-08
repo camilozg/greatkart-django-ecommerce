059d83ebd6ff8bf92c0df2761fda1858387a448f
camilozamora1@gmail.com <camilozamora1@gmail.com>
2023-07-08 01:22:05 -0500

    project updated

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -10,8 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import os
-import sys
 from pathlib import Path
 
 from django.contrib.messages import constants as messages
